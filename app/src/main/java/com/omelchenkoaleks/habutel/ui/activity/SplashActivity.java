4efd6fbd32e82451e39b2a6ce19314dc1c2a46fb
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-01-25 09:40:06 +0200

    Activity splash...

---
+++ app/src/main/java/com/omelchenkoaleks/habutel/ui/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.omelchenkoaleks.habutel.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.omelchenkoaleks.habutel.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
